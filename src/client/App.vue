60c450e2fe615ef54cf79ef9e9aae3d0a72be555
nvtem <nza@ro.ru>
2021-04-25 14:45:13 +0600

    Convert html to pug (App.vue)

--- src/client/App.vue
+++ src/client/App.vue
@@ -1,20 +1,19 @@
-<template>
-  <div class="app-root">
-    <router-view></router-view>
-    <div class="d-none">
-<!--LiveInternet counter--><a href="https://www.liveinternet.ru/click"
-target="_blank"><img id="licntF17D" width="88" height="31" style="border:0"
-title="LiveInternet: показано число просмотров и посетителей за 24 часа"
-src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAEALAAAAAABAAEAAAIBTAA7"
-alt=""/></a><script type="application/javascript">(function(d,s){d.getElementById("licntF17D").src=
-"https://counter.yadro.ru/hit?t52.6;r"+escape(d.referrer)+
-((typeof(s)=="undefined")?"":";s"+s.width+"*"+s.height+"*"+
-(s.colorDepth?s.colorDepth:s.pixelDepth))+";u"+escape(d.URL)+
-";h"+escape(d.title.substring(0,150))+";"+Math.random()})
-(document,screen)</script><!--/LiveInternet-->
+<template lang="pug">
+  .app-root
+    router-view
+    .d-none
+      // LiveInternet counter
+      a(href='https://www.liveinternet.ru/click' target='_blank')
+        img#licntF17D(width='88' height='31' style='border:0' title='LiveInternet: показано число просмотров и посетителей за 24 часа' src='data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAEALAAAAAABAAEAAAIBTAA7' alt='')
+      script(type='application/javascript').
+        (function(d,s){d.getElementById("licntF17D").src=
+          "https://counter.yadro.ru/hit?t52.6;r"+escape(d.referrer)+
+          ((typeof(s)=="undefined")?"":";s"+s.width+"*"+s.height+"*"+
+            (s.colorDepth?s.colorDepth:s.pixelDepth))+";u"+escape(d.URL)+
+          ";h"+escape(d.title.substring(0,150))+";"+Math.random()})
+        (document,screen)
+      // /LiveInternet
 
-    </div>
-  </div>
 </template>
 
 <style lang="stylus">
