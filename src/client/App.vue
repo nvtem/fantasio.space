ded30fd4a28d8f4ae9c93761f688abca5b35a3ce
Artem <nza@ro.ru>
2021-10-11 20:05:12 +0600

    Remove LiveInternet counter

--- src/client/App.vue
+++ src/client/App.vue
@@ -1,19 +1,6 @@
 <template lang="pug">
   .app-root
     router-view
-    .d-none
-      // LiveInternet counter
-      a(href='https://www.liveinternet.ru/click' target='_blank')
-        img#licntF17D(width='88' height='31' style='border:0' title='LiveInternet: показано число просмотров и посетителей за 24 часа' src='data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAEALAAAAAABAAEAAAIBTAA7' alt='')
-      script(type='application/javascript').
-        (function(d,s){d.getElementById("licntF17D").src=
-          "https://counter.yadro.ru/hit?t52.6;r"+escape(d.referrer)+
-          ((typeof(s)=="undefined")?"":";s"+s.width+"*"+s.height+"*"+
-            (s.colorDepth?s.colorDepth:s.pixelDepth))+";u"+escape(d.URL)+
-          ";h"+escape(d.title.substring(0,150))+";"+Math.random()})
-        (document,screen)
-      // /LiveInternet
-
 </template>
 
 <style lang="stylus">
